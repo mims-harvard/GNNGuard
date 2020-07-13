18228ad8b5af0b3f236a40ff45b0849d405086dc
Xiang Zhang <xiang.alan.zhang@gmail.com>
2020-07-13 10:53:40 +1000

    Add files via upload

---
+++ nn/conv/gin_conv.py
@@ -0,0 +1,57 @@
+import torch
+from torch_geometric.nn.conv import MessagePassing
+from torch_geometric.utils import remove_self_loops
+
+from ..inits import reset
+
+
+class GINConv(MessagePassing):
+    r"""The graph isomorphism operator from the `"How Powerful are
+    Graph Neural Networks?" <https://arxiv.org/abs/1810.00826>`_ paper
+
+    .. math::
+        \mathbf{x}^{\prime}_i = h_{\mathbf{\Theta}} \left( (1 + \epsilon) \cdot
+        \mathbf{x}_i + \sum_{j \in \mathcal{N}(i)} \mathbf{x}_j \right),
+
+    here :math:`h_{\mathbf{\Theta}}` denotes a neural network, *.i.e.* a MLP.
+
+    Args:
+        nn (torch.nn.Module): A neural network :math:`h_{\mathbf{\Theta}}` that
+            maps node features :obj:`x` of shape :obj:`[-1, in_channels]` to
+            shape :obj:`[-1, out_channels]`, *e.g.*, defined by
+            :class:`torch.nn.Sequential`.
+        eps (float, optional): (Initial) :math:`\epsilon` value.
+            (default: :obj:`0`)
+        train_eps (bool, optional): If set to :obj:`True`, :math:`\epsilon`
+            will be a trainable parameter. (default: :obj:`False`)
+        **kwargs (optional): Additional arguments of
+            :class:`torch_geometric.nn.conv.MessagePassing`.
+    """
+
+    def __init__(self, nn, eps=0, train_eps=False, **kwargs):
+        super(GINConv, self).__init__(aggr='add', **kwargs)
+        self.nn = nn
+        self.initial_eps = eps
+        if train_eps:
+            self.eps = torch.nn.Parameter(torch.Tensor([eps]))
+        else:
+            self.register_buffer('eps', torch.Tensor([eps]))
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        reset(self.nn)
+        self.eps.data.fill_(self.initial_eps)
+
+    def forward(self, x, edge_index, edge_weight=None):
+        """"""
+        x = x.unsqueeze(-1) if x.dim() == 1 else x
+        # """don't have operation on the self-loop because the self-loop already added in att_coe function"""
+        # edge_index, edge_weight = remove_self_loops(edge_index, edge_attr=edge_weight)
+        out = self.nn((1 + self.eps) * x + self.propagate(edge_index, x=x, norm=edge_weight))
+        return out
+
+    def message(self, x_j, norm):
+        return norm.view(-1, 1) *x_j
+
+    def __repr__(self):
+        return '{}(nn={})'.format(self.__class__.__name__, self.nn)
